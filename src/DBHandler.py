94ec18b7e846576850cd4afa6558629b6afd6c61
Guilherme Poleto <gpoleto@alunos.utfpr.edu.br>
2023-02-04 17:57:37 -0300

    Changed model definitions

--- src/DBHandler.py
+++ src/DBHandler.py
@@ -28,7 +28,7 @@ class Material(orm_sqlite.Model):
 
 class Body(orm_sqlite.Model):
     id = orm_sqlite.IntegerField(primary_key=True)
-    body_type = orm_sqlite.StringField()
+    body_type = orm_sqlite.IntegerField()
     material = orm_sqlite.ForeignKeyField(Material)
     param_a = orm_sqlite.FloatField()
     param_b = orm_sqlite.FloatField()
@@ -38,23 +38,24 @@ class Body(orm_sqlite.Model):
 class Experiment(orm_sqlite.Model):
     id = orm_sqlite.IntegerField(primary_key=True)
     name = orm_sqlite.StringField()
-    material = orm_sqlite.ForeignKeyField(Material)
+    body = orm_sqlite.ForeignKeyField(Body)
     date = orm_sqlite.DateField()
     time = orm_sqlite.TimeField()
     load_loss_limit = orm_sqlite.FloatField()
-    maxLoad = orm_sqlite.FloatField()
-    maxTravel = orm_sqlite.FloatField()
-    maxTime = orm_sqlite.TimeField()
+    max_load = orm_sqlite.FloatField()
+    max_travel = orm_sqlite.FloatField()
+    max_time = orm_sqlite.TimeField()
     compress = orm_sqlite.BooleanField()
     z_axis_speed = orm_sqlite.FloatField()
 
 
 class Reading(orm_sqlite.Model):
     id = orm_sqlite.IntegerField(primary_key=True)
+    index = orm_sqlite.IntegerField()
     experiment = orm_sqlite.ForeignKeyField(Experiment)
     load = orm_sqlite.FloatField()
     z_pos = orm_sqlite.FloatField()
-    time = orm_sqlite.TimeField()
+    tension = orm_sqlite.FloatField()
 
 
 class DBHandler:
@@ -69,83 +70,65 @@ class DBHandler:
         if not exists:
             self.populate()
 
+    # --- Material --- #
+
     def add_material(self, data: dict):
         material = Material(data)
         material.save()
 
-    def add_body(self, data: dict):
-        body = Body(data)
-        body.save()
-
-    def add_experiment(self, data: dict):
-        experiment = Experiment(data)
-        experiment.save()
-
-    def add_reading(self, data: dict):
-        reading = Reading(data)
-        reading.save()
-
     def get_materials(self):
         return Material.objects.all()
 
-    def get_material_by_id(self, id: int):
-        return Material.objects.get(pk=id)
-
-    def get_bodies(self):
-        return Body.objects.all()
-
-    def get_bodies_by_id(self, id: int):
-        return Body.objects.get(pk=id)
-
-    def get_bodies_by_material(self, material: Material):
-        return Body.objects.filter(material=material).all()
-
-    def get_bodies_by_type(self, body_type: str):
-        return Body.objects.filter(body_type=body_type).all()
-
-    def get_bodies_by_material_and_type(self, material: Material, body_type: str):
-        return Body.objects.filter(material=material, body_type=body_type).all()
-
-    def get_experiments(self):
-        return Experiment.objects.order_by(Experiment.date.desc()).all()
-
-    def get_experiment_by_id(self, id: int):
-        return Experiment.objects.get(pk=id)
-
-    def get_experiments_by_material(self, material: Material):
-        return (
-            Experiment.objects.filter(material=material)
-            .order_by(Experiment.date.desc())
-            .all()
-        )
-
-    def get_experiments_by_date(self, date: str):
-        return (
-            Experiment.objects.filter(date=date).order_by(Experiment.date.desc()).all()
-        )
-
-    def get_experiments_by_date_and_material(self, date: str, material: Material):
-        return (
-            Experiment.objects.filter(date=date, material=material)
-            .order_by(Experiment.date.desc())
-            .all()
-        )
-
-    def get_experiment_readings(self, experiment: Experiment):
-        return (
-            Reading.objects.filter(experiment=experiment)
-            .order_by(Reading.time.asc())
-            .all()
-        )
-
-    def delete_experiment_by_id(self, id: int):
-        Reading.objects.filter(experiment=id).delete()
-        Experiment.objects.get(pk=id).delete()
+    def get_materials_by_filter(self, filter: dict):
+        return Material.objects.filter(**filter).all()
 
     def delete_material_by_id(self, id: int):
         Reading.objects.filter(experiment__material=id).delete()
         Experiment.objects.filter(material=id).delete()
         Material.objects.get(pk=id).delete()
+
+    # --- Body --- #
+
+    def add_body(self, data: dict):
+        body = Body(data)
+        body.save()
+
+    def get_bodies(self):
+        return Body.objects.all()
+
+    def get_bodies_by_filter(self, filter: dict):
+        return Body.objects.filter(**filter).all()
+
+    def delete_body_by_id(self, id: int):
+        Experiment.objects.filter(body=id).delete()
+        Body.objects.get(pk=id).delete()
+
+    # --- Experiment --- #
+
+    def add_experiment(self, data: dict):
+        experiment = Experiment(data)
+        experiment.save()
+
+    def get_experiments(self):
+        return Experiment.objects.all()
+
+    def get_experiments_by_filter(self, filter: dict):
+        return Experiment.objects.filter(**filter).all()
+
+    def delete_experiment_by_id(self, id: int):
+        Reading.objects.filter(experiment=id).delete()
+        Experiment.objects.get(pk=id).delete()
+
+    # --- Reading --- #
+
+    def add_reading(self, data: dict):
+        reading = Reading(data)
+        reading.save()
+
+    def get_experiment_readings(self, experiment_id: int):
+        return Reading.objects.filter(experiment=experiment_id).all()
+
+    # --- Populate --- #
 
     def populate(self):
         self.db.create_tables([Material, Experiment, Reading])
@@ -154,43 +137,42 @@ class DBHandler:
         self.add_material({"name": "Iron", "batch": "I1"})
 
         for i in range(10):
-            self.add_experiment(
+            self.add_body(
                 {
-                    "name": "Test {}".format(i),
-                    "material": 1,
-                    "date": date.today(),
-                    "time": time(),
-                    "load_loss_limit": 0.1,
-                    "maxLoad": 100,
-                    "maxTravel": 100,
-                    "maxTime": time(),
-                    "compress": True,
-                    "z_axis_speed": 100,
+                    "body_type": 0,
+                    "material": i % 2 + 1,
+                    "param_a": 0.1 + i / 10,
+                    "param_b": 0.1 + 2 * i / 10,
+                    "height": 0.1 + 3 * i / 10,
                 }
             )
-            for j in range(10):
-                self.add_reading(
-                    {"experiment": i + 1, "load": j, "z_pos": j, "time": time()}
-                )
 
         for i in range(10):
             self.add_experiment(
                 {
-                    "name": "Test {}".format(i),
-                    "material": 2,
+                    "name": "Exp " + str(i),
+                    "body": i % 10 + 1,
                     "date": date.today(),
                     "time": time(),
-                    "load_loss_limit": 0.1,
-                    "maxLoad": 100,
-                    "maxTravel": 100,
-                    "maxTime": time(),
-                    "compress": True,
-                    "z_axis_speed": 100,
+                    "load_loss_limit": 0.1 + i / 10,
+                    "max_load": 0.1 + 2 * i / 10,
+                    "max_travel": 0.1 + 3 * i / 10,
+                    "max_time": time(),
+                    "compress": i % 2 == 0,
+                    "z_axis_speed": 0.1 + 4 * i / 10,
                 }
             )
+
+        for i in range(10):
             for j in range(10):
                 self.add_reading(
-                    {"experiment": i + 11, "load": j, "z_pos": j, "time": time()}
+                    {
+                        "index": j,
+                        "experiment": i % 10 + 1,
+                        "load": 0.1 + j / 10,
+                        "z_pos": 0.1 + 2 * j / 10,
+                        "tension": 0.1 + 3 * j / 10,
+                    }
                 )
 
         self.db.commit()
