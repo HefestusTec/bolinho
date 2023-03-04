816f919591e2d1a91f369a35a2c49272655b13d5
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-03-03 21:22:49 -0300

    added suport for PWA

--- src/web/src/index.js
+++ src/web/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 import "@fontsource/roboto";
 
