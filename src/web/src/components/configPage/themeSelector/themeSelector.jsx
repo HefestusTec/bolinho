b43085bb3ee73030dd4e334d009f596b324a71bf
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-05-04 21:43:17 -0300

    reformatted the divisions and spans, also started worrking in selector customization

---
+++ src/web/src/components/configPage/themeSelector/themeSelector.jsx
@@ -0,0 +1,84 @@
+// Copyright (C) 2023 Hefestus
+//
+// This file is part of Bolinho.
+//
+// Bolinho is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bolinho is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Bolinho.  If not, see <http://www.gnu.org/licenses/>.
+
+import React, { useContext, useState } from "react";
+//import styleModule from "./configPage.module.css";
+
+import ZoomComponent from "../../zoomComponent/zoomComponent";
+import ContainerComponent from "../../containerComponent/containerComponent";
+import GlobalConfigContext from "../../../contexts/globalConfigContext";
+import ButtonGroup from "../../customSubComponents/buttonGroup/buttonGroup";
+import CustomButton from "../../customSubComponents/customButton/customButton";
+
+export default function ThemeSelector({ className, scaleOrigin }) {
+    const [globalConfig, setGlobalConfig] = useContext(GlobalConfigContext);
+    const [currentActive, setCurrentActive] = useState(globalConfig.theme);
+
+    const clickCallBack = (key) => {
+        setCurrentActive(key);
+        setGlobalConfig({ ...globalConfig, theme: key });
+    };
+
+    return (
+        <ZoomComponent className={className} scaleOrigin={scaleOrigin}>
+            <ContainerComponent headerText="Temas">
+                <ButtonGroup
+                    currentActive={currentActive}
+                    clickCallBack={clickCallBack}
+                >
+                    <CustomButton
+                        key={"Claro"}
+                        buttonKey={"Claro"}
+                        color={"white"}
+                        iconSize="var(--font_s)"
+                        className=""
+                    >
+                        Claro
+                    </CustomButton>
+                    <CustomButton
+                        key={"Escuro"}
+                        buttonKey={"Escuro"}
+                        color={"white"}
+                        iconSize="var(--font_s)"
+                        className=""
+                    >
+                        Escuro
+                    </CustomButton>
+                    <CustomButton
+                        key={"PlaceHolder"}
+                        buttonKey={"PlaceHolder"}
+                        color={"white"}
+                        iconSize="var(--font_s)"
+                        className=""
+                    >
+                        PlaceHolder
+                    </CustomButton>
+
+                    <CustomButton
+                        key={"PlaceHolder2"}
+                        buttonKey={"PlaceHolder2"}
+                        color={"white"}
+                        iconSize="var(--font_s)"
+                        className=""
+                    >
+                        PlaceHolder2
+                    </CustomButton>
+                </ButtonGroup>
+            </ContainerComponent>
+        </ZoomComponent>
+    );
+}
---
+++ src/web/src/components/customSubComponents/customCheckbox/customCheckbox.jsx
@@ -0,0 +1,70 @@
+// Copyright (C) 2023 Hefestus
+//
+// This file is part of Bolinho.
+//
+// Bolinho is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bolinho is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Bolinho.  If not, see <http://www.gnu.org/licenses/>.
+
+import React from "react";
+import styleModule from "./customCheckbox.module.css";
+
+function CustomCheckbox({
+    preIcon,
+    postIcon,
+    color,
+    iconSize,
+    children,
+    className,
+    buttonKey,
+    clickCallBack,
+}) {
+    const buttonStyle = { "--font_color": color };
+    const iconStyle = {
+        color: color ? color : "#000000",
+        width: iconSize ? iconSize : "1em",
+        height: iconSize ? iconSize : "1em",
+        verticalAlign: "middle",
+    };
+
+    const createPreIcon = () => {
+        if (preIcon) return <preIcon.type style={iconStyle} />;
+        return;
+    };
+
+    const createPostIcon = () => {
+        if (postIcon) return <postIcon.type style={iconStyle} />;
+        return;
+    };
+
+    const getClassName = () => {
+        return [className, styleModule.custom_button].join(" ");
+    };
+
+    const clicked = () => {
+        if (clickCallBack !== undefined) {
+            clickCallBack(buttonKey);
+        }
+    };
+
+    return (
+        <button
+            className={getClassName()}
+            style={buttonStyle}
+            onClick={clicked}
+        >
+            {createPreIcon()} {children} {createPostIcon()}
+        </button>
+    );
+}
+
+export default CustomCheckbox;
