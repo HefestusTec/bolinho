663471bfc9f7d681f1fc4ba4b07935225a92fcb4
ZRafaF <ZRafaF@users.noreply.github.com>
2023-07-11 23:07:00 +0000

    deploy: b118d1a829c39ddfb6fe76683fa7046c95745ab6

---
+++ build/static/js/752.f23cbdb2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkweb=self.webpackChunkweb||[]).push([[752],{752:function(e,o,n){n.r(o),window.eel.expose((function(){return 1}),"pingJS"),window.eel.expose((function(){alert("Go to experiment")}),"goToExperimentPageJS"),window.eel.expose((function(){alert("Show connect prompt")}),"showConnectPromptJS");var t=n(85);try{window.eel.expose((function(e){t.Am.error(e)}),"toastErrorJS")}catch(c){}try{window.eel.expose((function(e){t.Am.success(e)}),"toastSuccessJS")}catch(c){}}}]);
+//# sourceMappingURL=752.f23cbdb2.chunk.js.map
