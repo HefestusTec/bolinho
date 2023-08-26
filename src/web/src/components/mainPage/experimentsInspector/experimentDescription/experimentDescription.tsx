59d59be3d553325ebb1226117d9bdb5018af22bf
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-08-26 17:17:58 -0300

    fixed exception throw

--- src/web/src/components/mainPage/experimentsInspector/experimentDescription/experimentDescription.tsx
+++ src/web/src/components/mainPage/experimentsInspector/experimentDescription/experimentDescription.tsx
@@ -29,123 +29,124 @@ const ExperimentDescription: FunctionComponent<ExperimentDescriptionProps> = ({
     experiment,
     material,
 }) => {
-    const materialDescription = (
-        <table>
-            <th>
-                <h1>Material</h1>
-            </th>
-            <th>
-                <br /> <br />
-            </th>
-            <tr>
-                <th>Nome</th>
-                <th>{material.name}</th>
-            </tr>
-            <tr>
-                <th>ID</th>
-                <th>{material.id}</th>
-            </tr>
-            <tr>
-                <th>Fornecedor</th>
-                <th>{material.supplier_name}</th>
-            </tr>
-            <tr>
-                <th>Informações do fornecedor</th>
-                <th>{material.supplier_contact_info}</th>
-            </tr>
-            <tr>
-                <th>Lote</th>
-                <th>{material.batch}</th>
-            </tr>
-            <tr>
-                <th>Extra</th>
-                <th>{material.extra_info}</th>
-            </tr>
-        </table>
-    );
-    const experimentDescription = (
-        <table>
-            <th>
-                <h1>Experimento</h1>
-            </th>
-            <th>
-                <br /> <br />
-            </th>
-            <tr>
-                <th>Nome</th>
-                <th>{experiment.name}</th>
-            </tr>
-            <tr>
-                <th>ID</th>
-                <th>{experiment.id}</th>
-            </tr>
-            <tr>
-                <th>Data</th>
-                <th>{experiment.date_time}</th>
-            </tr>
-            <tr>
-                <th>Limite de perca de carga</th>
-                <th>{experiment.load_loss_limit.toFixed(2)} N/s</th>
-            </tr>
-            <tr>
-                <th>Limite de carga</th>
-                <th>{experiment.max_load.toFixed(2)} N</th>
-            </tr>
-            <tr>
-                <th>Limite de distância</th>
-                <th>{experiment.max_travel.toFixed(2)} mm</th>
-            </tr>
-            <tr>
-                <th>Limite de tempo</th>
-                <th>{experiment.max_time.toFixed(2)} s</th>
-            </tr>
-            <tr>
-                <th>Velocidade do eixo Z</th>
-                <th>{experiment.z_axis_speed.toFixed(2)} mm/s</th>
-            </tr>
-            <tr>
-                <th>Tipo de exp.</th>
-                <th> {experiment.compress ? "Compressão" : "Expansão"}</th>
-            </tr>
-            <tr>
-                <th>Extra</th>
-                <th>{experiment.extra_info}</th>
-            </tr>
-        </table>
-    );
-    const bodyDescription = (
-        <table>
-            <th>
-                <h1>Corpo de prova</h1>
-            </th>
-            <th>
-                <br /> <br />
-            </th>
-
-            <tr>
-                <th>ID</th>
-                <th>{experiment.body.id}</th>
-            </tr>
-            <tr>
-                <th>Tipo</th>
-                <th>{getBodyTypeAsText(experiment.body.type)}</th>
-            </tr>
-            <tr>
-                <th>Altura</th>
-                <th>{experiment.body.height.toFixed(2)} mm</th>
-            </tr>
-            <tr>
-                <th>Parâmetro A</th>
-                <th>{experiment.body.param_a.toFixed(2)} mm</th>
-            </tr>
-            <tr>
-                <th>Parâmetro B</th>
-                <th>{experiment.body.param_b.toFixed(2)} mm</th>
-            </tr>
-        </table>
-    );
     const makeMaterialText = () => {
         if (experiment === undefined) return;
+
+        const materialDescription = (
+            <table>
+                <th>
+                    <h1>Material</h1>
+                </th>
+                <th>
+                    <br /> <br />
+                </th>
+                <tr>
+                    <th>Nome</th>
+                    <th>{material.name}</th>
+                </tr>
+                <tr>
+                    <th>ID</th>
+                    <th>{material.id}</th>
+                </tr>
+                <tr>
+                    <th>Fornecedor</th>
+                    <th>{material.supplier_name}</th>
+                </tr>
+                <tr>
+                    <th>Informações do fornecedor</th>
+                    <th>{material.supplier_contact_info}</th>
+                </tr>
+                <tr>
+                    <th>Lote</th>
+                    <th>{material.batch}</th>
+                </tr>
+                <tr>
+                    <th>Extra</th>
+                    <th>{material.extra_info}</th>
+                </tr>
+            </table>
+        );
+        const experimentDescription = (
+            <table>
+                <th>
+                    <h1>Experimento</h1>
+                </th>
+                <th>
+                    <br /> <br />
+                </th>
+                <tr>
+                    <th>Nome</th>
+                    <th>{experiment.name}</th>
+                </tr>
+                <tr>
+                    <th>ID</th>
+                    <th>{experiment.id}</th>
+                </tr>
+                <tr>
+                    <th>Data</th>
+                    <th>{experiment.date_time}</th>
+                </tr>
+                <tr>
+                    <th>Limite de perca de carga</th>
+                    <th>{experiment.load_loss_limit.toFixed(2)} N/s</th>
+                </tr>
+                <tr>
+                    <th>Limite de carga</th>
+                    <th>{experiment.max_load.toFixed(2)} N</th>
+                </tr>
+                <tr>
+                    <th>Limite de distância</th>
+                    <th>{experiment.max_travel.toFixed(2)} mm</th>
+                </tr>
+                <tr>
+                    <th>Limite de tempo</th>
+                    <th>{experiment.max_time.toFixed(2)} s</th>
+                </tr>
+                <tr>
+                    <th>Velocidade do eixo Z</th>
+                    <th>{experiment.z_axis_speed.toFixed(2)} mm/s</th>
+                </tr>
+                <tr>
+                    <th>Tipo de exp.</th>
+                    <th> {experiment.compress ? "Compressão" : "Expansão"}</th>
+                </tr>
+                <tr>
+                    <th>Extra</th>
+                    <th>{experiment.extra_info}</th>
+                </tr>
+            </table>
+        );
+        const bodyDescription = (
+            <table>
+                <th>
+                    <h1>Corpo de prova</h1>
+                </th>
+                <th>
+                    <br /> <br />
+                </th>
+
+                <tr>
+                    <th>ID</th>
+                    <th>{experiment.body.id}</th>
+                </tr>
+                <tr>
+                    <th>Tipo</th>
+                    <th>{getBodyTypeAsText(experiment.body.type)}</th>
+                </tr>
+                <tr>
+                    <th>Altura</th>
+                    <th>{experiment.body.height.toFixed(2)} mm</th>
+                </tr>
+                <tr>
+                    <th>Parâmetro A</th>
+                    <th>{experiment.body.param_a.toFixed(2)} mm</th>
+                </tr>
+                <tr>
+                    <th>Parâmetro B</th>
+                    <th>{experiment.body.param_b.toFixed(2)} mm</th>
+                </tr>
+            </table>
+        );
         return (
             <>
                 {materialDescription}
