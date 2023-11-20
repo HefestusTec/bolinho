34028a5f750d9763c0ac3a654f434444c961cdc4
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-11-20 13:14:07 -0300

    setup update

--- Bolinho/setup.sh
+++ Bolinho/setup.sh
@@ -2,6 +2,7 @@
 
 # Define paths and names
 APP_NAME="Bolinho"
+RELATIVE_PATH="$(dirname "$0")"
 START_SCRIPT_PATH="$(dirname "$0")/start.sh"
 LOGO_PATH="$(dirname "$0")/src/web/build/logo192.png"  # Assuming the logo is in the same directory as setup.sh
 DESKTOP_ENTRY_PATH="$HOME/.local/share/applications/Bolinho.desktop"
@@ -14,12 +15,12 @@ echo "Type=Application" >> "$DESKTOP_ENTRY_PATH"
 echo "Name=$APP_NAME" >> "$DESKTOP_ENTRY_PATH"
 echo "Exec=$START_SCRIPT_PATH" >> "$DESKTOP_ENTRY_PATH"
 echo "Icon=$LOGO_PATH" >> "$DESKTOP_ENTRY_PATH"
-echo "Terminal=false" >> "$DESKTOP_ENTRY_PATH"
+echo "Terminal=true" >> "$DESKTOP_ENTRY_PATH"
 
 # Make it executable
 chmod +x "$DESKTOP_ENTRY_PATH"
 
 # Copy desktop entry to Desktop
-cp "$DESKTOP_ENTRY_PATH" "$DESKTOP_SHORTCUT_DIR"
+cp "$DESKTOP_ENTRY_PATH" "$RELATIVE_PATH/"
 
 echo "Instalação completa. Bolinho agora está acessivel no menu."
