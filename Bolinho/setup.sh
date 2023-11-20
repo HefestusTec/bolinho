9413655db2393db6b41d7f796a7cf1d0f9deeeb8
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-11-20 12:35:11 -0300

    added setup script

---
+++ Bolinho/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define paths and names
+APP_NAME="Bolinho"
+START_SCRIPT_PATH="$(dirname "$0")/start.sh"
+LOGO_PATH="$(dirname "$0")/src/web/build/logo192.png"  # Assuming the logo is in the same directory as setup.sh
+DESKTOP_ENTRY_PATH="$HOME/.local/share/applications/Bolinho.desktop"
+DESKTOP_SHORTCUT_DIR="$HOME/Desktop"
+
+# Create the .desktop file
+echo "[Desktop Entry]" > "$DESKTOP_ENTRY_PATH"
+echo "Version=1.0" >> "$DESKTOP_ENTRY_PATH"
+echo "Type=Application" >> "$DESKTOP_ENTRY_PATH"
+echo "Name=$APP_NAME" >> "$DESKTOP_ENTRY_PATH"
+echo "Exec=$START_SCRIPT_PATH" >> "$DESKTOP_ENTRY_PATH"
+echo "Icon=$LOGO_PATH" >> "$DESKTOP_ENTRY_PATH"
+echo "Terminal=false" >> "$DESKTOP_ENTRY_PATH"
+
+# Make it executable
+chmod +x "$DESKTOP_ENTRY_PATH"
+
+# Copy desktop entry to Desktop
+cp "$DESKTOP_ENTRY_PATH" "$DESKTOP_SHORTCUT_DIR"
+
+echo "Instalação completa. Bolinho agora está acessivel no menu."
